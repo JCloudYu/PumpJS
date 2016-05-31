e763c2c280214d8095e2508cb1c11846bc55210d
JCloudYu <j.cloud.yu@gmail.com>
2016-06-01 02:20:20 +0800

    Add session-based unique token generator

--- util/misc.js
+++ util/misc.js
@@ -2,6 +2,31 @@
 	"use strict";
 
 	var
+	___UNIQUE_ID = (function(){
+		var hexStr = [ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' ];
+		return function( base, time, seq, rand ) {
+			var
+			result = '',
+			bytes = [
+				(base >> 8) & 255,
+				(base	  ) & 255,
+
+				(time >> 24) & 255,
+				(time >> 16) & 255,
+				(time >> 8 ) & 255,
+				(time	   ) & 255,
+
+				seq & 255,
+
+				(rand >> 8) & 255,
+				rand & 255
+			];
+
+			bytes.forEach(function( value ){ result += hexStr[ (value >> 4) & 15 ] + hexStr[ value & 15 ]; });
+
+			return result;
+		};
+	})(),
 	__prevO	= window.PumpMisc;
 
 	window._$PM = window.PumpMisc = function( targetObject ){};
@@ -11,7 +36,16 @@
 
 
 	___MERGE( window._$PM, {
-		merge: ___MERGE
+		merge: ___MERGE,
+		getId: (function(){
+			var
+			base = (Math.random() * 65536) | 0,
+			seq	 = 0;
+
+			return function(){
+				return ___UNIQUE_ID( base, (new Date()).getTime() | 0, seq = (seq++) & 255, (Math.random() * 65536) );
+			};
+		})()
 	});
 
 	function ___MERGE( obj1, obj2, overwrite, iterated ) {
